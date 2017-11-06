e20de2b4d63e48d6d7c4b153111466be24bf3791
Alastair Otter <alastair@mediahack.co.za>
2017-11-06 16:48:32 +0200

    updating server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // content of index.js
-
+var config = require('./config');
 var fs = require('fs');
 var logs;
 
@@ -35,7 +35,7 @@ fs.readFile('log.txt', 'utf8', function(err, data) {
 
 setInterval( function () { 
     
-        fs.readFile('log.txt', 'utf8', function(err, data) {
+        fs.readFile(config.baseDir + 'log.txt', 'utf8', function(err, data) {
               if (err) throw err;
             //  console.log('OK: ' + filename);
         //      console.log(data)
